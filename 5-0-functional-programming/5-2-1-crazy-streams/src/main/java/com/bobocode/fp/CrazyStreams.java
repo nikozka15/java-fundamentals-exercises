341b8cf37148fe6b77f8bfe03e0c5f77f83f86c2
nikozka15 <vshyrshova@gmail.com>
2024-07-13 22:02:55 +0100

    train

--- 5-0-functional-programming/5-2-1-crazy-streams/src/main/java/com/bobocode/fp/CrazyStreams.java
+++ 5-0-functional-programming/5-2-1-crazy-streams/src/main/java/com/bobocode/fp/CrazyStreams.java
@@ -1,5 +1,6 @@
 package com.bobocode.fp;
 
+import com.bobocode.fp.exception.EntityNotFoundException;
 import com.bobocode.model.Account;
 import com.bobocode.util.ExerciseNotCompletedException;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,10 @@ import lombok.AllArgsConstructor;
 import java.math.BigDecimal;
 import java.time.Month;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * {@link CrazyStreams} is an exercise class. Each method represent some operation with a collection of accounts that
@@ -30,7 +35,7 @@ public class CrazyStreams {
      * @return account with max balance wrapped with optional
      */
     public Optional<Account> findRichestPerson() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream().max(Comparator.comparing(Account::getBalance));
     }
 
     /**
@@ -40,7 +45,7 @@ public class CrazyStreams {
      * @return a list of accounts
      */
     public List<Account> findAccountsByBirthdayMonth(Month birthdayMonth) {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream().filter(a -> a.getBirthday().getMonth() == birthdayMonth).toList();
     }
 
     /**
@@ -50,7 +55,7 @@ public class CrazyStreams {
      * @return a map where key is true or false, and value is list of male, and female accounts
      */
     public Map<Boolean, List<Account>> partitionMaleAccounts() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream().collect(Collectors.partitioningBy(a -> a.getSex().name().equals("MALE")));
     }
 
     /**
@@ -60,7 +65,7 @@ public class CrazyStreams {
      * @return a map where key is an email domain and value is a list of all account with such email
      */
     public Map<String, List<Account>> groupAccountsByEmailDomain() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream().collect(Collectors.groupingBy(a -> a.getEmail().split("@")[1]));
     }
 
     /**
@@ -69,7 +74,7 @@ public class CrazyStreams {
      * @return total number of letters of first and last names of all accounts
      */
     public int getNumOfLettersInFirstAndLastNames() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream().mapToInt(a -> Math.addExact(a.getFirstName().length(), a.getLastName().length())).sum();
     }
 
     /**
@@ -78,7 +83,7 @@ public class CrazyStreams {
      * @return total balance of all accounts
      */
     public BigDecimal calculateTotalBalance() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream().map(Account::getBalance).reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     /**
@@ -87,7 +92,10 @@ public class CrazyStreams {
      * @return list of accounts sorted by first and last names
      */
     public List<Account> sortByFirstAndLastNames() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream()
+                .sorted(Comparator.comparing((Account account) -> account.getFirstName())
+                .thenComparing(Comparator.comparing(Account::getLastName)))
+                        .toList();
     }
 
     /**
@@ -97,7 +105,7 @@ public class CrazyStreams {
      * @return true if there is an account that has an email with provided domain
      */
     public boolean containsAccountWithEmailDomain(String emailDomain) {
-        throw new ExerciseNotCompletedException();
+       return accounts.stream().anyMatch(a -> a.getEmail().contains(emailDomain));
     }
 
     /**
@@ -108,7 +116,11 @@ public class CrazyStreams {
      * @return account balance
      */
     public BigDecimal getBalanceByEmail(String email) {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream()
+                .filter(a -> a.getEmail().equals(email))
+                .findAny()
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Cannot find Account by email=%s", email)))
+                .getBalance();
     }
 
     /**
@@ -117,7 +129,7 @@ public class CrazyStreams {
      * @return map of accounts by its ids
      */
     public Map<Long, Account> collectAccountsById() {
-        throw new ExerciseNotCompletedException();
+      return accounts.stream().collect(Collectors.toMap((Account::getId), a -> a));
     }
 
     /**
@@ -128,7 +140,9 @@ public class CrazyStreams {
      * @return map of account by its ids the were created in a particular year
      */
     public Map<String, BigDecimal> collectBalancesByEmailForAccountsCreatedOn(int year) {
-        throw new ExerciseNotCompletedException();
+       return accounts.stream()
+               .filter(a -> a.getCreationDate().getYear() == year)
+               .collect(Collectors.toMap((Account::getEmail), (Account::getBalance)));
     }
 
     /**
@@ -138,7 +152,11 @@ public class CrazyStreams {
      * @return a map where key is a last name and value is a set of first names
      */
     public Map<String, Set<String>> groupFirstNamesByLastNames() {
-        throw new ExerciseNotCompletedException();
+       return accounts.stream()
+               .collect(Collectors.groupingBy(
+                       (Account::getLastName),
+                       Collectors.mapping(Account::getFirstName, Collectors.toSet())
+               ));
     }
 
     /**
@@ -148,7 +166,10 @@ public class CrazyStreams {
      * @return a map where a key is a birthday month and value is comma-separated first names
      */
     public Map<Month, String> groupCommaSeparatedFirstNamesByBirthdayMonth() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream()
+                .collect(Collectors.groupingBy(
+                        a -> a.getBirthday().getMonth(),
+                        Collectors.mapping(Account::getFirstName, Collectors.joining(", "))));
     }
 
     /**
@@ -158,7 +179,9 @@ public class CrazyStreams {
      * @return a map where key is a creation month and value is total balance of all accounts created in that month
      */
     public Map<Month, BigDecimal> groupTotalBalanceByCreationMonth() {
-        throw new ExerciseNotCompletedException();
+       return accounts.stream()
+                .collect(Collectors.groupingBy(a -> a.getCreationDate().getMonth(),
+                        Collectors.mapping(Account::getBalance, Collectors.reducing(BigDecimal.ZERO, BigDecimal::add))));
     }
 
     /**
@@ -168,7 +191,9 @@ public class CrazyStreams {
      * @return a map where key is a letter and value is its count in all first names
      */
     public Map<Character, Long> getCharacterFrequencyInFirstNames() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream()
+                .flatMap(a -> a.getFirstName().chars().mapToObj(c -> (char) c))
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
     }
 
     /**
@@ -179,7 +204,12 @@ public class CrazyStreams {
      * @return a map where key is a letter and value is its count ignoring case in all first and last names
      */
     public Map<Character, Long> getCharacterFrequencyIgnoreCaseInFirstAndLastNames(int nameLengthBound) {
-        throw new ExerciseNotCompletedException();
+         return accounts.stream()
+                 .flatMap(a-> Stream.of(a.getFirstName(), a.getLastName()))
+                 .map(String::toLowerCase)
+                 .flatMapToInt(String::chars)
+                 .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
 }
--- lesson-demo/src/main/java/com/bobocode/DemoApp.java
+++ lesson-demo/src/main/java/com/bobocode/DemoApp.java
@@ -1,6 +1,96 @@
 package com.bobocode;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.math3.util.Pair;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+
+@Slf4j
 public class DemoApp {
-    public static void main(String[] args) {
+    private static final String API_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=DEMO_KEY";
+
+    public static void main(String[] args) throws IOException {
+        String jsonResponse = fetchDataFromAPI(API_URL);
+        log.info(jsonResponse);
+        List<String> urls = parseImageUrls(jsonResponse);
+
+        Pair<Long, String> largeSizeAndUrl = findLargestImage(urls);
+        log.info(largeSizeAndUrl.getFirst() + " " + largeSizeAndUrl.getSecond());
+    }
+
+    private static String fetchDataFromAPI(String apiUrl) throws IOException {
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        InputStream responseStream = connection.getInputStream();
+        try (Scanner scanner = new Scanner(responseStream, StandardCharsets.UTF_8)) {
+            return scanner.useDelimiter("\\A").next();
+        }
+    }
+
+    private static List<String> parseImageUrls(String jsonResponse) {
+        JSONObject jsonObject = new JSONObject(jsonResponse);
+        JSONArray photosArray = jsonObject.getJSONArray("photos");
+
+        return IntStream.range(0, photosArray.length())
+                .mapToObj(photosArray::getJSONObject)
+                .map(photoObj -> photoObj.getString("img_src"))
+                .toList();
+    }
+
+    private static Pair<Long, String> findLargestImage(List<String> imageUrls) throws IOException {
+        String largestImageUrl = null;
+        long largestSize = 0;
+
+        for (String imageUrl : imageUrls) {
+            long imageSize = getImageSize(imageUrl);
+            log.info("Image link {} size: {}", imageUrl, imageSize);
+            if (imageSize > largestSize) {
+                largestSize = imageSize;
+                largestImageUrl = imageUrl;
+            }
+        }
+
+        return new Pair<>(largestSize, largestImageUrl);
+    }
+
+    private static long getImageSize(String imageUrl) throws IOException {
+        HttpURLConnection connection = null;
+
+        try {
+            URL url = new URL(imageUrl);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setInstanceFollowRedirects(true);
+            connection.setRequestMethod("HEAD");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK || responseCode == HttpURLConnection.HTTP_MOVED_TEMP || responseCode == HttpURLConnection.HTTP_MOVED_PERM) {
+                if (responseCode == HttpURLConnection.HTTP_MOVED_TEMP || responseCode == HttpURLConnection.HTTP_MOVED_PERM) {
+                    String newUrl = connection.getHeaderField("Location");
+                    if (newUrl != null) {
+                        connection.disconnect();
+                        return getImageSize(newUrl);
+                    }
+                }
+                return connection.getContentLengthLong();
+            } else {
+                throw new IOException("Error fetching image size, response code: " + responseCode);
+            }
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
     }
 }
