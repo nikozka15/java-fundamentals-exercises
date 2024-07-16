b9d02a8be18398d9fdee770c93f1bae9aece08e3
Taras Boychuk <tboychuk@bobocode.com>
2024-07-16 06:35:13 +0300

    Warmup Task * implement a custom method `comparing` & `max`

--- lesson-demo/src/main/java/com/bobocode/DemoApp.java
+++ lesson-demo/src/main/java/com/bobocode/DemoApp.java
@@ -1,6 +1,20 @@
 package com.bobocode;
 
+import com.bobocode.data.Accounts;
+import com.bobocode.model.Account;
+
+
 public class DemoApp {
     public static void main(String[] args) {
+        var accounts = Accounts.generateAccountList(10);
+//        Account maxAccount = max(accounts, comparing(Account::getFirstName));
+//        System.out.println(maxAccount);
     }
+
+    // todo: DISABLE AI Assistant
+
+    // todo: implement a custom method comparing
+
+    // todo: implement a custom method max
+
 }
